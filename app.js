09365cbee2b13c5b594593de3345f70447ae57bc
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-10 18:25:11 +0530

    select element in js

---
+++ app.js
@@ -0,0 +1,5 @@
+let value = document.querySelector('#value');
+let decrease = document.querySelector('.decrease');
+let increase = document.querySelector('.increase');
+let reset = document.querySelector('.reset')
+
