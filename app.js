381281091a47b538e468d8b559b6a0c79d0b3774
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-10 18:25:44 +0530

    add functions

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ let decrease = document.querySelector('.decrease');
 let increase = document.querySelector('.increase');
 let reset = document.querySelector('.reset')
 
+let count = 0;
+
+updateDisplay();
+
+increase.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+decrease.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
+
+reset.addEventListener("click",()=>{
+    count = 0;
+    updateDisplay();
+})
+
+function updateDisplay(){
+    value.innerHTML = count;
+};
